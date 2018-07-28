ebeb9680b4788edc157bd5d1076b78cc3513d25e
Dustin Schnee <schnee72@gmail.com>
2018-07-28 16:14:11 -0500

    working on vuex

--- src/store.js
+++ src/store.js
@@ -3,9 +3,45 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+const state = {
+  count: 0
+};
+
+const mutations = {
+  increment (state) {
+    state.count++;
+  },
+  decrement (state) {
+    state.count--;
+  }
+};
+
+const actions = {
+  increment: ({ commit }) => commit('increment'),
+  decrement: ({ commit }) => commit('decrement'),
+  incrementIfOdd ({ commit, state }) {
+    if ((state.count + 1) % 2 === 0) {
+      commit('increment');
+    }
+  },
+  incrementAsync ({ commit }) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        commit('increment');
+        resolve();
+      }, 1000);
+    });
+  }
+};
+
+const getters = {
+  evenOrOdd: state => state.count % 2 === 0 ? 'even' : 'odd'
+};
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-	modules: {},
-	strict: true
+  state,
+  getters,
+  actions,
+  mutations
 });
+
