89643bf4a44a7f2a396ffc04cd88f9b853704190
Dustin Schnee <schnee72@gmail.com>
2018-07-28 19:37:09 -0500

    hook up store

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,18 +2,35 @@
   <div id="about">
     <h2>about</h2>
     <p>{{something}}</p>
+    <div>{{ $store.state.count }}</div>
+    <div>{{ evenOrOdd }}</div>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">Increment if odd</button>
+    <button @click="incrementAsync">Increment async</button>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+
 export default {
   name: 'About',
   data() {
     return { something: 'This is about something!' };
   },
+  computed: mapGetters([
+    'evenOrOdd'
+  ]),
   mounted() {
     console.log('about!');
-  }
+  },
+  methods: mapActions([
+    'increment',
+    'decrement',
+    'incrementIfOdd',
+    'incrementAsync'
+  ])
 };
 </script>
 
